3830b88c0dc45eda62f036fd5d8d1aa8c4077a7b
seojinsarejj <s19009@naver.com>
2020-12-02 19:58:48 +0900

    [CREATE] Create project create modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Menu from './components/Menu/Menu';
 import Navbar from './components/NavBar/Navbar';
+import Modal from './components/NavBar/modal';
 import { Component } from 'react';
 
 class App extends Component {
@@ -12,6 +13,8 @@ class App extends Component {
       <div className="App">
         <Menu/>
         <Navbar/>
+        <Modal visible={true}>Hello</Modal>
+
       </div>
     );
   }
