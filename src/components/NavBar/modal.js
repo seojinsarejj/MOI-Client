a9e70eab71e26caa7e992a813ef91d58a16f8f26
seojinsarejj <s19009@naver.com>
2020-12-10 00:09:45 +0900

    [UPDATE] Update create project modal title

--- src/components/NavBar/modal.js
+++ src/components/NavBar/modal.js
@@ -12,15 +12,16 @@ function Modal({ className, visible, children, close }) {
           <div className="modalHeader">
             <button onClick={close}>X</button>
           </div>
-          <div className="title">CREATE PROJECT</div>
           <div className = "createModal">
+
+            <div className="title">CREATE PROJECT</div>
             <div>
               <input className="InputBox" type="text" placeholder=" 프로젝트 이름"></input>
               <input className="InputBox" type="text" placeholder=" 사용한 기술"></input>
               <input className="ContentInputBox" type="text" placeholder=" 프로젝트 설명"></input>
-              <input className="InputBox" type="text" placeholder="GITHUB"></input>
+              <input className="InputBox" type="text" placeholder=" GITHUB"></input>
             </div>
-            <div>dd</div>
+            <div><button className = "transBtn">완료</button></div>
           </div>
           
         </ModalInner>
